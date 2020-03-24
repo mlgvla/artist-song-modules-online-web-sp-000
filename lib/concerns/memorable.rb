b5a33c6364ece9f47745407ae6491b9d02376f7f
Monica Gerard <mlgerardvla@gmail.com>
2020-03-24 22:52:55 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,13 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
 
-  def reset_all
-    all.clear
-  end
+    def count
+      all.count
+    end
 
-  def count
-    all.count
-  end
+    module InstanceMethods
 
 end
